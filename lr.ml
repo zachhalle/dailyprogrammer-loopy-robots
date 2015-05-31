c1ef10a3581a0cb50911c521415cf93d683423ff
zachhalle <zhalle@cmu.edu>
2015-05-31 14:48:27 -0400

    Update lr.ml

--- lr.ml
+++ lr.ml
@@ -6,12 +6,9 @@ let left d = match d with | N -> W | W -> S | S -> E | E -> N
 let right d = match d with | N -> E | E -> S | S -> W | W -> N
 
 let step ((x, y), d) c = match c, d with
-  | 'S', N -> ((x, y+1), d)
-  | 'S', S -> ((x, y-1), d)
-  | 'S', E -> ((x+1, y), d)
-  | 'S', W -> ((x-1, y), d)
-  | 'L', _ -> ((x,y), left d)
-  | 'R', _ -> ((x,y), right d)
+  | 'S', N -> ((x, y+1), d) | 'S', S -> ((x, y-1), d)
+  | 'S', E -> ((x+1, y), d) | 'S', W -> ((x-1, y), d)
+  | 'L', _ -> ((x,y), left d) | 'R', _ -> ((x,y), right d)
   | _      -> failwith "Invalid input"
 
 let run s = String.fold s ~init:((0,0), N) ~f:step
